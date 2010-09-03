cd5d45a87ecb31131b95d9e44cbdb6e8497d06c1
Eelco Wesemann <eelco.wesemann@gmail.com>
2010-09-03 13:55:55 +0200

    Importing changes towards PHPrbl version 0.3

--- rbl.php
+++ rbl.php
@@ -6,14 +6,14 @@
 #
 # (c) Eelco Wesemann (eelco@init1.nl)
 # http://phprbl.init1.nl || http://eol.init1.nl
-# (Version 0.2, May 10 2005)
+# (Version 0.3, May 16 2005)
 
 # Configuration section
 
 # The following aray contains the RBL services we want to use
 # Spamhaus and AHBL are tested to be quite fast, you can 
 # add your own, or remove unwanted services from the array.
-$rbl_services = array ('sbl-xbl.spamhaus.org', 'dnsbl.ahbl.org');
+$rbl_services = array ('rbl.init1.nl', 'sbl-xbl.spamhaus.org', 'dnsbl.ahbl.org');
 
 # set $mysql_enable to 1 if you want to log blocked hosts to mysql
 # please note that the table structure has changed between version 0.1 
@@ -21,11 +21,15 @@ $rbl_services = array ('sbl-xbl.spamhaus.org', 'dnsbl.ahbl.org');
 # README for more info.
 $mysql_enable = 0;
 
+# Set $mysql_precheck to 1 if you want to check an IP address against the local
+# database first
+$mysql_precheck = 0;
+
 # if $mysql_enable is 1, you will need to enter the following information
-$mysql_host = "MYSQLHOST";
-$mysql_user = "MYSQLUSER";
-$mysql_pass = "MYSQLPASS";
-$mysql_data = "MYSQLDATA";
+$mysql_host = "MYSQLHOST";		# mysql host (usually localhost)
+$mysql_user = "MYSQLUSER";		# mysql username
+$mysql_pass = "MYSQLPASS";		# mysql password
+$mysql_data = "MYSQLDATA";		# mysql database
 
 #
 # there should be no real need to edit anything below this
@@ -49,39 +53,17 @@ $timestamp = time();
 # this is a quick and easy match
 $pattern = '/127.0.0.?/';
 
-
-$matches = 0;
-$service = "";
-foreach ($rbl_services as $check) {
-	$lookup_rbl_ip = implode('.', $reverse_ip) . '.' . $check;
-	$do_lookup = gethostbyname($lookup_rbl_ip);
-	if (preg_match($pattern, $do_lookup, $pat_match)) {
-		$matches++;
-		$service .= "$check;";
-	}
-}
-
-if ($matches > 0) {
-	if ($mysql_enable == 1) {
-		$mysql_link = mysql_connect("$mysql_host", "$mysql_user", "$mysql_pass") or die("Unable to connect to database");
-		mysql_select_db($mysql_data, $mysql_link) or die ("Unable to select database"); 
-		
-		$query_ip = mysql_query("SELECT count FROM blocked WHERE ip='$client_ip'", $mysql_link);
-		if ($row = mysql_fetch_array($query_ip)) {
-			$count = $row[0];
-			$count++;
-			mysql_query("UPDATE blocked SET lastseen='$timestamp', service='$service', count='$count', referer='$referer' WHERE ip='$client_ip'");
-		} else {
-			# We haven't seen the IP address yet, and will insert it for the first time with a count of 1
-			mysql_query("INSERT INTO blocked (ip,lastseen,service,count,referer) values ('$client_ip', '$timestamp', '$service', '1', '$referer')");
-		}
-
-		mysql_close($mysql_link);
-	}
-
+# function blockpage:
+# arguments: client ip address, services IP is listed in.
+# Pass the 403 Forbidden Header, and show a bit of HTML explaining
+# what is going on, and why the page is not loading.
+function blockpage ($client_ip,$service) {
 	header("HTTP/1.0 403 Forbidden");
+	echo "<html><head>\n";
+	echo "<title>403 Forbidden - PHPrbl</title>\n";
+	echo "</head><body>\n";
 	echo "<h1>403 Forbidden</h1><br />\n";
-	echo "Your client IP ($client_ip - $lookup_client_ip) is listed as an open proxy at the following services:<br />\n";
+	echo "Your client IP ($client_ip) is listed as an open proxy or abusive host at the following services:<br />\n";
 	echo "<ul>\n";
 	$blockedby = explode(";", $service);
 	foreach ($blockedby as $rbl) {
@@ -91,9 +73,61 @@ if ($matches > 0) {
 	}
 	echo "</ul>\n";
 	echo "Users of open proxies are unwanted on this site because of various types of SPAM.<br />\n";
-	echo "IP address denied and thus the page is exiting";
-	echo "<br /><br />";
-	exit("This site is protected against open proxies by <a href=\"http://phprbl.init1.nl\">PHPrbl</a>.");
+	echo "IP address denied and thus the page is exiting<br /><br />\n";
+	exit("This site is protected against open proxies and abusive hosts by <a href=\"http://phprbl.init1.nl\">PHPrbl</a>.\n</body></html>");
+}
+#end function blockpage
+
+if ($mysql_enable == 1) {
+	$mysql_link = mysql_connect("$mysql_host", "$mysql_user", "$mysql_pass") or die("Unable to connect to database");
+	mysql_select_db($mysql_data, $mysql_link) or die ("Unable to select database");
 }
 
+if ($mysql_enable == 1 && $mysql_precheck ==1) {
+	$query_count_ip = mysql_query("SELECT count(ip) AS precheck_ip,service,count FROM blocked WHERE ip='$client_ip' GROUP BY service");
+	while($row = mysql_fetch_object($query_count_ip)) {
+		$precheck_ip = $row->precheck_ip;
+		$service = $row->service;
+		$count = $row->count;
+	}
+	if ($precheck_ip > 0) {
+		if (!preg_match("/localmysql/i", $service)) {
+			$service .= "localmysql;";
+		}
+		$count++;
+		mysql_query("UPDATE blocked SET lastseen='$timestamp', service='$service', count='$count', referer='$referer' WHERE ip='$client_ip'");
+		mysql_close($mysql_link);
+		blockpage($client_ip,$service);
+	}
+} else {
+	$matches = 0;
+	$service = "";
+	foreach ($rbl_services as $check) {
+		$lookup_rbl_ip = implode('.', $reverse_ip) . '.' . $check;
+		$do_lookup = gethostbyname($lookup_rbl_ip);
+		if (preg_match($pattern, $do_lookup, $pat_match)) {
+			$matches++;
+			$service .= "$check;";
+		}
+	}
+
+	if ($matches > 0) {
+		if ($mysql_enable == 1) {
+			$query_ip_hits = mysql_query("SELECT count FROM blocked WHERE ip='$client_ip'");
+			while($row = mysql_fetch_object($query_ip_hits)) {
+				$count = $row->count;
+			}
+			if ($count > 0) {
+				# count > 0; This means we know the IP already and have to raise it by 1
+				$count++;
+				mysql_query("UPDATE blocked SET lastseen='$timestamp', service='$service', count='$count', referer='$referer' WHERE ip='$client_ip'");
+			} else {
+				# count = 0; We haven't seen the IP address yet, and will insert it for the first time with a count of 1
+				mysql_query("INSERT INTO blocked (ip,lastseen,service,count,referer) values ('$client_ip', '$timestamp', '$service', '1', '$referer')");
+			}
+			mysql_close($mysql_link);
+		}
+		blockpage($client_ip,$service);
+	}
+}
 ?>
